ae9521fee573b387540f486f803992e8fd015b01
Andrew Davisson <akdavisson4@gmail.com>
2020-01-31 22:42:09 -0700

    wrote test for longest word

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 let expect = require ("chai").expect;
 let palindromeTest = require("../app/palindrome");
+let longestWord = require("../app/longestWord");
 
 describe("Palindrome Test", function() {
   describe("returns 'Palindrome' when the word is a palindrome", function() {
@@ -34,5 +35,17 @@ describe("Palindrome Test", function() {
       let test3 = palindromeTest.palindromeTest("GOODBYE");
       expect(test3).to.equal("Not a Palindrome");
     });
+  })
+});
+
+describe("Longest Word Test", function(){
+  it("returns the longest word of a string", function(){
+    let word1 = longestWord.longestWord("a dog is smelly");
+    let word2 = longestWord.longestWord("blue yellow green red");
+    let word3 = longestWord.longestWord("Henry Sue Jake");
+
+    expect(word1).to.equal("smelly");
+    expect(word2).to.equal("yellow");
+    expect(word3).to.equal("Henry");
   })
 });
